e12aaa4eebf3fbdda326c44fe0a41cdaffdaea76
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-10-11 00:02:34 +0900

    Java언어 BAEKJOON 10171번 문제 풀기!(1)

---
+++ Java_10171_1/Java_10171_1.java
@@ -0,0 +1,16 @@
+/*
+아래 예제와 같이 고양이를 출력하시오.
+
+없음.
+
+고양이를 출력한다.
+*/
+
+class Main{
+    public static void main(String args[]){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");        
+    }
+}

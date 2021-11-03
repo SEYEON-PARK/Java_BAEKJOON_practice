c91d91ce549f74bfb4fbc1c79d8ac54f3d00ee8c
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-11-04 06:27:03 +0900

    Java언어 BAEKJOON 5596번 문제 풀기!(2)

---
+++ Java_5596_2/Java_5596_2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner one = new Scanner(System.in);
+        int s[], t[], S=0, T=0;
+        s=new int [4];
+        t=new int [4];
+        
+        for(int i=0;i<4;i++){
+            s[i]=one.nextInt();
+            S+=s[i];
+        }
+        
+        for(int i=0;i<4;i++){
+            t[i]=one.nextInt();
+            T+=t[i];
+        }
+        
+        if(S>=T){
+            System.out.println(S);
+        }
+        else{
+            System.out.println(T);
+        }
+        
+        one.close();
+    }
+}

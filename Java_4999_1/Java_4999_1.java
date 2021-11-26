185d75cf9cb7cabedeeddd380f1b2ce500e27c7a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-11-27 06:42:03 +0900

    Java언어 BAEKJOON 4999번 문제 풀기!(1)

--- Java_4999_1/Java_4999_1.java
+++ Java_4999_1/Java_4999_1.java
@@ -12,3 +12,31 @@
 재환이가 그 병원에 가야하면 "go"를, 아니면 "no"를 출력한다.
 */
 
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner a=new Scanner(System.in);
+        
+        char [] j=a.next().toCharArray();
+        char [] d=a.next().toCharArray();
+        int j_num=0, d_num=0;
+        
+        for(int i=0;j[i]!='h';i++){
+            j_num++;
+        }
+        
+        for(int i=0;d[i]!='h';i++){
+            d_num++;
+        }
+        
+        if(j_num>=d_num){
+            System.out.println("go");
+        }
+        else{
+            System.out.println("no");
+        }
+        
+        a.close();
+    }
+}

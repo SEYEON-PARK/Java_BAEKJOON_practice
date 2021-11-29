e23d8ce295d7204e4c548779e994969f465f8231
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-11-30 06:47:46 +0900

    Java언어 BAEKJOON 2741번 문제 풀기!(1)

--- Java_2741_1/Java_2741_1.java
+++ Java_2741_1/Java_2741_1.java
@@ -6,10 +6,10 @@
 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
 */
 
-import java.util.*;
+import java.util.*; // 스캐너를 사용하기 위해
 
-class Main{
-    public static void main(String args[]){
+class Main{ // Main 클래스
+    public static void main(String args[]){ // main 메소드
         Scanner s=new Scanner(System.in);
         int N=s.nextInt();
         

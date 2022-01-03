7ead31d9bdcb2d1fe96839679893992e6cf1eae6
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-04 06:24:39 +0900

    Java언어 BAEKJOON 8393번 문제 풀기!(2)

--- Java_8393_2/Java_8393_2.java
+++ Java_8393_2/Java_8393_2.java
@@ -5,3 +5,21 @@ n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성�
 
 1부터 n까지 합을 출력한다.
 */
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+       Scanner a=new Scanner(System.in);
+       int N=a.nextInt();
+       int sum=0;
+        
+       for (int i=1;i<=N;i++){
+           sum+=i;
+       }
+        
+       System.out.println(sum);
+        
+       a.close();
+    }
+}

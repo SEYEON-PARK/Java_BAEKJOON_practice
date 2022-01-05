ed6fdf9b9666bd6f4c0b863730001b4b06673ba3
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-06 06:33:09 +0900

    Java언어 BAEKJOON 8393번 문제 풀기!(3)

---
+++ Java_8393_3/Java_8393_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+       Scanner a=new Scanner(System.in);
+       int N=a.nextInt();
+       int sum=0;
+        
+       for (int i=N;i>0;i--){
+           sum+=i;
+       }
+        
+       System.out.println(sum);
+        
+       a.close();
+    }
+}

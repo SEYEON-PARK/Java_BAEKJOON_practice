9281c2f0836228fe21408053d2fd93468e86002d
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-07 06:49:20 +0900

    Java언어 BAEKJOON 8393번 문제 풀기!(3)

--- Java_8393_3/Java_8393_3.java
+++ Java_8393_3/Java_8393_3.java
@@ -14,12 +14,12 @@ class Main{ // Main 클래스
        int N=a.nextInt(); // 사용자로부터 정수 입력받기
        int sum=0; // 정수 선언 및 초기화
         
-       for (int i=N;i>0;i--){
-           sum+=i;
+       for (int i=N;i>0;i--){ // i가 N부터 0보다 클 때까지 1씩 감소하며 반복 
+           sum+=i; // sum에 sum+i 대입하기
        }
         
-       System.out.println(sum);
+       System.out.println(sum); // 결과 출력하기
         
-       a.close();
+       a.close(); // 스캐너 a 닫기
     }
 }

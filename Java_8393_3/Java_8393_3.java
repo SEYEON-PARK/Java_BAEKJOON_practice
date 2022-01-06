564c7d5b9d9b7b039b26c78b9bea2fe19f2b00f8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-07 06:48:14 +0900

    Java언어 BAEKJOON 8393번 문제 풀기!(3)

--- Java_8393_3/Java_8393_3.java
+++ Java_8393_3/Java_8393_3.java
@@ -8,11 +8,11 @@ n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성�
 
 import java.util.Scanner; // 스캐너를 사용하기 위해
 
-class Main{
-    public static void main(String args[]){
-       Scanner a=new Scanner(System.in);
-       int N=a.nextInt();
-       int sum=0;
+class Main{ // Main 클래스
+    public static void main(String args[]){ // main 메소드
+       Scanner a=new Scanner(System.in); // 새로운 스캐너 a 생성
+       int N=a.nextInt(); // 사용자로부터 정수 입력받기
+       int sum=0; // 정수 선언 및 초기화
         
        for (int i=N;i>0;i--){
            sum+=i;

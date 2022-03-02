197372e3964072150bed5242a684f3a28ecbd056
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-03-02 11:17:15 +0900

    Java언어 BAEKJOON 11021번 문제 풀기!(2)

--- Java_11021_2/Java_11021_2.java
+++ Java_11021_2/Java_11021_2.java
@@ -7,9 +7,9 @@
 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
 */
 
-import java.util.*;
+import java.util.*; // 스캐너를 사용하기 위해서
 
-class Main{
+class Main{ // Main 클래스
     public static void main(String srg[]){
         Scanner s = new Scanner(System.in);
         int N=s.nextInt();

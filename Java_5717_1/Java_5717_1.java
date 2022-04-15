0159aa020937960d6df6f1bdaf0c3470af387ad0
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-04-16 00:29:58 +0900

    Java언어 BAEKJOON 5717번 문제 풀기!(1)

--- Java_5717_1/Java_5717_1.java
+++ Java_5717_1/Java_5717_1.java
@@ -6,3 +6,23 @@
 
 각 테스트 케이스마다 상근이의 친구의 수를 출력한다.
 */
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        int M, F;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            M=sc.nextInt();
+            F=sc.nextInt();
+            if(M==0 && F==0){
+                break;
+            }
+            System.out.println(M+F);
+        }
+        
+        sc.close();
+    }
+
+}

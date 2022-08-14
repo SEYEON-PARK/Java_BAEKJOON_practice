129c9de2097e6505daf70ec4e9b4cc040d420867
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-08-15 00:43:19 +0900

    Java언어 BAEKJOON 25083번 문제 풀기!(1)

--- Java_25083_1/Java_25083_1.java
+++ Java_25083_1/Java_25083_1.java
@@ -8,11 +8,11 @@
 
 class Main{
     public static void main(String args[]){
-        System.out.println("         ,r'\"7"); // 새싹 출력하기
+        System.out.println("         ,r'\"7");
         System.out.println("r`-_   ,'  ,/");
         System.out.println(" \\. \". L_r'");
         System.out.println("   `~\\/");
         System.out.println("      |");
-        System.out.println("      |");
+        System.out.println("      |"); // 새싹 출력하기
     }
 }

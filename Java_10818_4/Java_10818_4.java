145c527c0b99995aa67c9771688c4fbcdda4eb1f
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-08-23 01:52:02 +0900

    Java언어 BAEKJOON 10818번 문제 풀기!(4)

--- Java_10818_4/Java_10818_4.java
+++ Java_10818_4/Java_10818_4.java
@@ -12,15 +12,15 @@ import java.util.*; // 스캐너를 사용하기 위해
 class Main{ // Main 클래스
     public static void main(String args[]){ // main 메소드
         Scanner s = new Scanner(System.in); // 스캐너 s 생성
-        int N=s.nextInt();
-        int max, min;
+        int N=s.nextInt(); // 사용자로부터 정수 입력받기
+        int max, min; // 변수 선언
         int n[];
-        n = new int [1000000];
+        n = new int [1000000]; // 배열 선언
         
-        for(int i=0;i<N;i++)
-            n[i]=s.nextInt();
+        for(int i=0;i<N;i++) // i가 0부터 N보다 작을 때까지 1씩 증가하며 반복
+            n[i]=s.nextInt(); // 사용자로부터 정수를 입력받아 n[i]에 대입하기
         
-        max=n[0]; min=n[0];
+        max=n[0]; min=n[0]; // max에 n[0]의 값 대입하고, min에 n[0]의 값 대입하기
         
         for(int i=0;i<N;i++)
             if(n[i]>max)

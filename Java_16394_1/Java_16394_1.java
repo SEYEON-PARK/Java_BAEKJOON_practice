a6d9482d8a7611fbd4a4cd09941c9807c5350b88
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-03-15 09:02:10 +0900

    Java언어 BAEKJOON 16394번 문제 풀기!(1)

--- Java_16394_1/Java_16394_1.java
+++ Java_16394_1/Java_16394_1.java
@@ -13,9 +13,9 @@ import java.util.*; // 스캐너를 사용하기 위해
 
 class Main{ // Main 클래스
     public static void main(String [] args){ // main 메소드
-        Scanner s=new Scanner(System.in);
-        int y;
-        y=s.nextInt();
+        Scanner s=new Scanner(System.in); // 스캐너 s 생성
+        int y; // 변수 선언
+        y=s.nextInt(); // 사용자로부터 정수 입력받기
         
         System.out.println(y-1946);
     }

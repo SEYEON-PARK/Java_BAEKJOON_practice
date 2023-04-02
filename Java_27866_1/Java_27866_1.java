a15ce84aca1c7352730c9a03b77689db38472460
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-04-03 00:14:46 +0900

    Java언어 BAEKJOON 27866번 문제 풀기!(1)

--- Java_27866_1/Java_27866_1.java
+++ Java_27866_1/Java_27866_1.java
@@ -7,14 +7,14 @@
 S의 i번째 글자를 출력한다.
 */
 
-import java.util.*;
+import java.util.*; // 스캐너를 사용하기 위해
 
-class Main{
-    public static void main(String args[]){
-        Scanner s = new Scanner(System.in);
+class Main{ // Main 클래스
+    public static void main(String args[]){ // main 메소드
+        Scanner s = new Scanner(System.in); // 스캐너 s 생성
         
-        String st = s.next();
-        int n = s.nextInt();
+        String st = s.next(); // 사용자로부터 문자열 입력받기
+        int n = s.nextInt(); // 사용자로부터 정수 입력받기
         
         System.out.println(st.charAt(n-1));
         

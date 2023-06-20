8f10ed043b02be7cc93bfca86d7311bedd2a3e54
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-06-20 21:50:22 +0900

    Java언어 BAEKJOON 11022번 문제 풀기!(1)

--- Java_11022_1/Java_11022_1.java
+++ Java_11022_1/Java_11022_1.java
@@ -6,3 +6,20 @@
 
 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
 */
+
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int A, B, T; 
+        T=s.nextInt();
+    
+        for(int i=0;i<T;i++) // i가 0부터 T보다 작을 때까지 1씩 증가하며 반복
+        {
+            A=s.nextInt();
+            B=s.nextInt();
+            System.out.printf("Case #%d: %d + %d = %d\n", i+1, A, B, A+B); // 형식에 맞춰 결과 출력하기
+        }
+    }
+}

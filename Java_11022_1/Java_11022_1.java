188081572770868c94fb69983f23f71d3042a3c1
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-06-21 23:03:05 +0900

    Java언어 BAEKJOON 11022번 문제 풀기!(1)

--- Java_11022_1/Java_11022_1.java
+++ Java_11022_1/Java_11022_1.java
@@ -11,13 +11,13 @@ import java.util.*; // 스캐너를 사용하기 위해
 
 class Main{ // Main 클래스
     public static void main(String args[]){ // main 메소드
-        Scanner s = new Scanner(System.in);
-        int A, B, T; 
-        T=s.nextInt();
+        Scanner s = new Scanner(System.in); // 스캐너 s 생성
+        int A, B, T; // 변수 선언
+        T=s.nextInt(); // 사용자로부터 정수 입력받기
     
         for(int i=0;i<T;i++) // i가 0부터 T보다 작을 때까지 1씩 증가하며 반복
         {
-            A=s.nextInt();
+            A=s.nextInt(); // 사용자로부터 정수 입력받기
             B=s.nextInt();
             System.out.printf("Case #%d: %d + %d = %d\n", i+1, A, B, A+B); // 형식에 맞춰 결과 출력하기
         }

127d4a84874c24d21ad78fd26eebf9b397be9719
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-08-26 22:17:34 +0900

    Java언어 BAEKJOON 7287번 문제 풀기!(1)

--- Java_7287_1/Java_7287_1.java
+++ Java_7287_1/Java_7287_1.java
@@ -5,3 +5,10 @@
 
 첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
 */
+
+class Main{
+    public static void main(String args []){
+        System.out.println("144");
+        System.out.print("fourleaf");
+    }
+}

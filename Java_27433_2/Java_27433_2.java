8f0b6b0f7603bf54313bb9325270148ccf59fd88
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-11-23 01:29:33 +0900

    Java언어 BAEKJOON 27433번 문제 풀기!(2)

--- Java_27433_2/Java_27433_2.java
+++ Java_27433_2/Java_27433_2.java
@@ -15,10 +15,10 @@ class Main{ // Main 클래스
         System.out.println(fac(N)); // 결과(fac(N)의 리턴값) 출력하기
     }
     
-    public static long fac(int n){
-        if(n<=1)
-            return 1;
-        else
-            return n * fac(n-1);
+    public static long fac(int n){ // fac() 함수 정의(반환값은 long형이어야만 하고, static으로 정의해야만 한다!)
+        if(n<=1) // 만약, n이 1보다 작거나 같다면
+            return 1; // 1 반환하기
+        else // n이 1보다 작거나 같지 않다면(크다면)
+            return n * fac(n-1); // n * fac(n-1)의 값 대입하기
     }
 }

ea498c6661b9861da50b93132b84bee45db5f68d
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-11-24 22:35:31 +0900

    Java언어 BAEKJOON 9498번 문제 풀기!(1)

--- Java_9498_1/Java_9498_1.java
+++ Java_9498_1/Java_9498_1.java
@@ -5,3 +5,24 @@
 
 시험 성적을 출력한다.
 */
+
+import java.util.*;
+
+class Main{
+    public static void main(String arg[]){
+        Scanner s = new Scanner(System.in);
+        int score = s.nextInt();
+        
+        if(score>=90)
+            System.out.println("A");
+        else if(score>=80)
+            System.out.println("B");
+        else if(score>=70)
+            System.out.println("C");
+        else if(score>=60)
+            System.out.println("D");
+        else
+            System.out.println("F");
+        
+    }
+}

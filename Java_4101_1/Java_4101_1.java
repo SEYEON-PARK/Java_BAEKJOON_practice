c6e7257e88927dcbcace8cf4ce3b76763327ebf7
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-11-29 02:22:46 +0900

    Java언어 BAEKJOON 4101번 문제 풀기!(1)

--- Java_4101_1/Java_4101_1.java
+++ Java_4101_1/Java_4101_1.java
@@ -7,10 +7,10 @@
 각 테스트 케이스마다, 첫 번째 수가 두 번째 수보다 크면 Yes를, 아니면 No를 한 줄에 하나씩 출력한다.
 */
 
-import java.util.*;
+import java.util.*; // 스캐너를 사용하기 위해
 
-class Main{
-    public static void main(String [] arg){
+class Main{ // Main 클래스
+    public static void main(String [] arg){ // main 메소드
         Scanner s = new Scanner(System.in);
         int A = s.nextInt();
         int B = s.nextInt();

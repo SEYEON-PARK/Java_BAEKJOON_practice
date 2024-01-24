022ebc6159bca7e16891da6395cf17b65a903b4e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-01-25 02:04:39 +0900

    Java언어 BAEKJOON 2920번 문제 풀기!(1)

--- Java_2920_1/Java_2920_1.java
+++ Java_2920_1/Java_2920_1.java
@@ -20,7 +20,7 @@ class Main{ // Main 클래스
             num[i]=s.nextInt(); // 사용자로부터 정수를 입력받아 num[i]에 저장
         }
         
-        for(int i=0;i<8;i++){
+        for(int i=0;i<8;i++){ // i가 0부터 8이 되기 전까지 1씩 증가시키며 반복
             if(num[i]==i+1){ // 만약, num[i]가 i+1과 같다면
                 if(i==0) // 만약, i가 0이라면(첫 번째 숫자라면)
                     status = 1; // status에 1 대입하기(오름차순의 가능성이 있음을 표시)
@@ -32,23 +32,23 @@ class Main{ // Main 클래스
                     System.out.println("ascending"); // "ascending" 출력하기
                 continue; // 계속하기
             }
-            else if(num[i]==8-i){
-                if(i==0)
-                    status = 2;    
-                if(status != 2){
-                    System.out.println("mixed");
-                    break;
+            else if(num[i]==8-i){ // 만약 num[i]가 i+1과 같지 않고, 8-i와 같다면
+                if(i==0) // 만약, i가 0이라면(첫 번째 숫자라면)
+                    status = 2; // status에 2 대입하기(내림차순의 가능성이 있음을 표시)
+                if(status != 2){ // 만약, status가 2가 아니라면(첫 시작이 내림차순으로 시작되지 않았고, 다른 숫자로 시작되었다는 의미) 
+                    System.out.println("mixed"); // "mixed" 출력하기
+                    break; // 반복문 빠져 나가기
                 }
-                if(i==7)
-                    System.out.println("descending");
-                continue;
+                if(i==7) // 만약, i가 7이라면(마지막 숫자라면)
+                    System.out.println("descending"); // "descending" 출력하기
+                continue; // 계속하기
             }
-            else{
-                System.out.println("mixed");
-                break;
+            else{ // 만약 num[i]가 i+1과 같지 않고, 8-i와도 같지 않다면
+                System.out.println("mixed"); // "mixed" 출력하기
+                break; // 반복문 빠져 나가기
             }
         }
         
-        s.close();
+        s.close(); // 스캐너 s 종료하기
     }
 }

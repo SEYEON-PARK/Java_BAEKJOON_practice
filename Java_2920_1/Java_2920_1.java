ffd0bcb2478d61cb62f7e7947c658d58abc3c17c
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-01-25 02:00:26 +0900

    Java언어 BAEKJOON 2920번 문제 풀기!(1)

--- Java_2920_1/Java_2920_1.java
+++ Java_2920_1/Java_2920_1.java
@@ -21,16 +21,16 @@ class Main{ // Main 클래스
         }
         
         for(int i=0;i<8;i++){
-            if(num[i]==i+1){
-                if(i==0)
-                    status = 1;    
-                if(status != 1){
-                    System.out.println("mixed");
-                    break;
+            if(num[i]==i+1){ // 만약, num[i]가 i+1과 같다면
+                if(i==0) // 만약, i가 0이라면(첫 번째 숫자라면)
+                    status = 1; // status에 1 대입하기(오름차순의 가능성이 있음을 표시)
+                if(status != 1){ // 만약, status가 1이 아니라면(첫 시작이 오름차순으로 시작되지 않았고, 다른 숫자로 시작되었다는 의미) 
+                    System.out.println("mixed"); // "mixed" 출력하기
+                    break; // 반복문 빠져 나가기
                 }
-                if(i==7)
-                    System.out.println("ascending");
-                continue;
+                if(i==7) // 만약, i가 7이라면(마지막 숫자라면)
+                    System.out.println("ascending"); // "ascending" 출력하기
+                continue; // 계속하기
             }
             else if(num[i]==8-i){
                 if(i==0)

4ce8c99effcf9ba02d6965e1755e8cd0719e53a9
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-01-26 02:20:57 +0900

    Java언어 BAEKJOON 2920번 문제 풀기!(2)

--- Java_2920_2/Java_2920_2.java
+++ Java_2920_2/Java_2920_2.java
@@ -7,3 +7,50 @@
 
 첫째 줄에 ascending, descending, mixed 중 하나를 출력한다.
 */
+
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int num[] = new int[8];
+        int ascending[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        int descending[] = {8, 7, 6, 5, 4, 3, 2, 1};
+        int status = 0;
+        
+        for(int i=0;i<8;i++){
+            num[i]=s.nextInt();
+        }
+        
+        for(int i=0;i<8;i++){
+            if(num[i]==ascending[i]){
+                if(i==0)
+                    status = 1;    
+                if(status != 1){
+                    System.out.println("mixed");
+                    break;
+                }
+                if(i==7)
+                    System.out.println("ascending");
+                continue;
+            }
+            else if(num[i]==descending[i]){
+                if(i==0)
+                    status = 2;    
+                if(status != 2){
+                    System.out.println("mixed");
+                    break;
+                }
+                if(i==7)
+                    System.out.println("descending");
+                continue;
+            }
+            else{
+                System.out.println("mixed");
+                break;
+            }
+        }
+        
+        s.close();
+    }
+}

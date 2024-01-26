6b0f43ec7a018e92f1162ff2a820828502e0a6fe
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-01-27 03:44:08 +0900

    Java언어 BAEKJOON 2920번 문제 풀기!(2)

--- Java_2920_2/Java_2920_2.java
+++ Java_2920_2/Java_2920_2.java
@@ -36,21 +36,21 @@ class Main{ // Main 클래스
             }
             else if(num[i]==descending[i]){ // 만약 num[i]가 ascending[i]와 같지 않고, descending[i]와 같다면
                 if(i==0) // 만약, i가 0이라면(첫 번째 숫자라면)
-                    status = 2;    
-                if(status != 2){
-                    System.out.println("mixed");
-                    break;
+                    status = 2; // status에 2 대입하기(내림차순의 가능성이 있음을 표시)
+                if(status != 2){ // 만약, status가 2가 아니라면(첫 시작이 내림차순으로 시작되지 않았고, 다른 숫자로 시작되었다는 의미) 
+                    System.out.println("mixed"); // "mixed" 출력하기
+                    break; // 반복문 빠져 나가기
                 }
-                if(i==7)
-                    System.out.println("descending");
-                continue;
+                if(i==7) // 만약, i가 7이라면(마지막 숫자라면)
+                    System.out.println("descending"); // "descending" 출력하기
+                continue; // 계속하기
             }
-            else{
-                System.out.println("mixed");
-                break;
+            else{ // 만약 num[i]가 ascending[i]와 같지 않고, descending[i]와도 같지 않다면
+                System.out.println("mixed"); // "mixed" 출력하기
+                break; // 반복문 빠져 나가기
             }
         }
         
-        s.close();
+        s.close(); // 스캐너 s 종료하기
     }
 }

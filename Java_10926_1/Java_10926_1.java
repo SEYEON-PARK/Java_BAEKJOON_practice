ed1e114e21b0f8dd7e636417c17d422f9d2d3d8f
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-14 00:13:24 +0900

    Java언어 BAEKJOON 10926번 문제 풀기!(1)

--- Java_10926_1/Java_10926_1.java
+++ Java_10926_1/Java_10926_1.java
@@ -11,8 +11,8 @@ import java.util.*; // 스캐너를 사용하기 위해
 
 class Main{ // Main 클래스
     public static void main(String [] args){ // main 메소드
-        Scanner s = new Scanner(System.in);
-        String name = s.next();
+        Scanner s = new Scanner(System.in); // 스캐너 s 생성
+        String name = s.next(); // 사용자로부터 문자열 입력받기
         
         System.out.println(name+"??!");
         

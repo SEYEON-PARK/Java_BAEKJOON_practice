6418eeb0ea1a9747b03c487b0ccea160a792ca32
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-15 01:22:28 +0900

    Java언어 BAEKJOON 10869번 문제 풀기!(1)

--- Java_10869_1/Java_10869_1.java
+++ Java_10869_1/Java_10869_1.java
@@ -5,3 +5,21 @@
 
 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
 */
+
+import java.util.*;
+
+class Main{
+    public static void main(String args []){
+        Scanner s = new Scanner(System.in);
+        int A = s.nextInt();
+        int B = s.nextInt();
+        
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+        
+        s.close();
+    }
+}

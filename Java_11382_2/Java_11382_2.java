55e210875239b3ed49db17f83780893a734f714a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-10-03 11:59:44 +0900

    Java언어 BAEKJOON 11382번 문제 풀기!(2)

--- Java_11382_2/Java_11382_2.java
+++ Java_11382_2/Java_11382_2.java
@@ -6,19 +6,19 @@
 A+B+C의 값을 출력한다.
 */
 
-import java.util.*;
+import java.util.*; // 스캐너를 사용하기 위해
 
-class Main{
-    public static void main(String args[]){
-        Scanner s = new Scanner(System.in);
-        long numbers[] = new long[3];
+class Main{ // Main 클래스
+    public static void main(String args[]){ // main 메소드
+        Scanner s = new Scanner(System.in); // 스캐너 s 생성
+        long numbers[] = new long[3]; // long형 배열 numbers 선언
         
-        for(int i=0;i<3;i++){
-            numbers[i] = s.nextLong();
+        for(int i=0;i<3;i++){ // i에 0부터 3보다 작을 때까지 1씩 증가시킨 값을 대입하며 반복
+            numbers[i] = s.nextLong(); // 사용자로부터 정수 입력받기
         }
         
-        System.out.println(numbers[0]+numbers[1]+numbers[2]);
+        System.out.println(numbers[0]+numbers[1]+numbers[2]); // 세 정수 더하여 출력하기
         
-        s.close();
+        s.close(); // 스캐너 s 닫기
     }
 }
